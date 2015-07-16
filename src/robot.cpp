43b7aa926fcf592224f6ad0ac331f44c81663e3e
werehyena <morton.scott.e@gmail.com>
2015-07-15 20:56:07 -0400

    added more controls

--- src/robot.cpp
+++ src/robot.cpp
@@ -11,6 +11,7 @@ class Robot: public IterativeRobot
 private:
 
 	//PWM's
+	static const int EYE_PWM = 0;
 	static const int FRONT_LEFT_PWM = 1;
 	static const int FRONT_RIGHT_PWM = 2;
 	static const int BACK_LEFT_PWM = 3;
@@ -18,13 +19,14 @@ private:
 	static const int SMOKE_CANNON_PWM = 5;
 	static const int JAW_MOTOR_PWM = 6;
 	static const int HEAD_MOTOR_PWM = 7;
-	static const int LEFT_EYE_X_PWM = 8;
-	static const int RIGHT_EYE_X_PWM = 9;
+	static const int WING_FLAP_PWM = 8;
+	static const int WING_FOLD_PWM = 9;
+
 	static const int SMOKE_MACHINE_RELAY = 1;
 
-//	static const int TOPHEAD_LIMIT_PIN = 4;
-//	static const int BOTTOMJAW_LIMIT_PIN = 2;
-//	static const int CRASH_LIMIT_PIN = 3;
+	//Speeds
+	static constexpr float WING_FOLD_SPEED = 0.9;
+	static constexpr float WING_FLAP_SPEED = 0.5;
 
 	static const int SOUND_PIN = 2;
 	std::vector<int> sound_seq;
@@ -33,6 +35,13 @@ private:
 	RobotDrive *drive;
 
 	GamepadF310 *pilot;
+	GamepadF310 *copilot;
+
+	Victor *smoke_cannon;
+	Victor *jaw;
+	Victor *head;
+	Victor *wing_flap;
+	Victor *wing_fold;
 
 public:
 	void RobotInit()
@@ -45,8 +54,16 @@ public:
 		);
 
 		pilot = new GamepadF310(0);
+		copilot = new GamepadF310(1);
+
 		sound_out = new DigitalOutput(SOUND_PIN);
 		sound_out->Set(0);
+
+		smoke_cannon = new Victor(SMOKE_CANNON_PWM);
+		jaw = new Victor(JAW_MOTOR_PWM);
+		head = new Victor(HEAD_MOTOR_PWM);
+		wing_flap = new Victor(WING_FLAP_PWM);
+		wing_fold = new Victor(WING_FOLD_PWM);
 	}
 
 	void setSound(DigitalOutput *out, bool state) {
@@ -76,6 +93,13 @@ public:
         float rot = pilot->RightX();
 
         drive->MecanumDrive_Cartesian(x,y,rot);
+
+        if(copilot->ButtonState(GamepadF310::buttonStart)){
+        	wing_fold->Set(WING_FOLD_SPEED);
+        } else if(copilot->ButtonState(GamepadF310::buttonBack)) {
+        	wing_fold->Set( -WING_FOLD_SPEED );
+        }
+
 
         while (pilot->GetButtonEvent(&evt)) {
         	if (evt.button == GamepadF310::buttonA && evt.pressed)
